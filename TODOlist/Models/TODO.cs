a838a923009d2cbc8aa1ae1371d911dec2800d0a
Leonardo Gaspar Saheb <leosaheb2003@gmail.com>
2024-08-16 23:03:30 -0300

    MVC .NET

---
+++ TODOlist/Models/TODO.cs
@@ -0,0 +1,9 @@
+public class TODO{
+    public int id {get; set;}
+    public string Tarefa {get; set; } = string.Empty;
+
+    public string Descricao {get; set; } = string.Empty;
+
+    public bool Feto { get; set; } = false;
+
+}
