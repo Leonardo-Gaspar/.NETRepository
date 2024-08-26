b9f897c8a133a705d251266e1febf89cae872ea0
Leonardo Gaspar Saheb <leosaheb2003@gmail.com>
2024-08-26 00:29:12 -0300

    Update project

--- FirstOne/obj/Debug/net8.0/FirstOne.AssemblyInfo.cs
+++ FirstOne/obj/Debug/net8.0/FirstOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a838a923009d2cbc8aa1ae1371d911dec2800d0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0630dae551193f7237312af8993e86dc6e654e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
